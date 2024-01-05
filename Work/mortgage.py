14c3e3aabf71da798e601ce53436719e303cd99f
Sumit Pokharel <psumitcoding@gmail.com>
2024-01-06 02:14:27 +0545

    added exercise 1.9 and 1.10 answers (principal different than the website sample towards the end)

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,20 +1,13 @@
 # mortgage.py
-#
-# Exercise 1.7
+
+# Exercise 1.9 and 1.10 answers
 
 """
-Enter this program and run it. You should get an answer of 966,279.6.
-
-DONE.
-"""
-
-"""
-Suppose Dave pays an extra $1000/month for the first 12 months of the mortgage?
-
-Modify the program to incorporate this extra payment and have it print the total amount paid
-along with the number of months required.
-
-When you run the new program, it should report a total payment of 929,965.62 over 342 months.
+TO ME IN THE FUTURE: Resulting principal here is different than the website sample towards the end.
+I tried debugging with GPT-4 as well. The principal still stays different than the website's
+principal towards the end. Basically dunno why, but I believe this may be something to do with
+the hideous formatting of the code. I'll leave as it is for now. Maybe I'll know a better way
+to format my code when I revisit this again during the revision stage.
 """
 
 principal = 500000.0
@@ -22,15 +15,26 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 months = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 
-while principal > 0:
+while principal > 0 and months < extra_payment_start_month:
+    principal = (principal * (1+rate/12) - payment)
+    total_paid = (total_paid + payment)
     months += 1
-    while months <= 12:
-        principal = principal * (1+rate/12) - payment - 1000
-        total_paid = round((total_paid + payment + 1000), 7)
-        months += 1
+    print(f"{months} {total_paid:.2f} {principal:.2f}")
 
-    principal = principal * (1+rate/12) - payment
-    total_paid = round((total_paid + payment), 7) #rounded to 7 digits to get what the question asked for
+while principal > 0 and months <= extra_payment_end_month:
+    principal = (principal * (1+rate/12) - payment - 1000)
+    total_paid = (total_paid + payment + 1000)
+    months += 1
+    print(f"{months} {total_paid:.2f} {principal:.2f}")
 
-print('Total paid', total_paid, 'over', months, 'months.')
+while principal > 0 and months > extra_payment_end_month:
+    principal = (principal * (1+rate/12) - payment)
+    total_paid = (total_paid + payment)
+    months += 1
+    print(f"{months} {total_paid:.2f} {principal:.2f}")
+
+print(f"Total paid {total_paid:.2f} \nMonths {months}")
