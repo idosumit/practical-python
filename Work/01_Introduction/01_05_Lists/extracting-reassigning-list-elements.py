f5bc4ffb47b80671396d40a7d49379bc27d27f72
idosumit <psumitcoding@gmail.com>
2024-12-16 10:01:02 +0900

    middle of 1.5 - lists exercise

---
+++ Work/01_Introduction/01_05_Lists/extracting-reassigning-list-elements.py
@@ -0,0 +1,17 @@
+symbols = 'HPQ, AAPL, IBM, MSFT, YHOO, DOA, GOOG'
+
+symlist = symbols.split(',')
+
+print(symlist)
+
+symlist[2] = 'AIG'
+
+print(symlist)
+
+mysyms = []
+mysyms.append('GOOG')
+print(mysyms)
+
+symlist[-2:] = mysyms
+print(symlist)
+print(mysyms)
